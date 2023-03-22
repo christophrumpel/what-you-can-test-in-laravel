f10f3b6824ac5d6527323d3e80830364b63666d0
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-03-22 09:50:11 +0100

    Add testing view

--- tests/Feature/Pages/PageHomeTest.php
+++ tests/Feature/Pages/PageHomeTest.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
+it('returns correct view', function() {
+    // Act & Assert
+    $this->get('/')
+        ->assertOk()
+        ->assertViewIs('home');
+});
+
 it('lists released products', function () {
     // Arrange
     $releasedProduct = Product::factory()
